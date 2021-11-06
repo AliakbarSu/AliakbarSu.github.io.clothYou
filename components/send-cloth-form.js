03b3fc416da3e4966ddd8d6d03634d735c388c94
AliakbarSu <aliakbar.su@hotmail.com>
2021-11-07 10:56:09 +1300

    Update send-cloth form design

--- components/send-cloth-form.js
+++ components/send-cloth-form.js
@@ -30,9 +30,26 @@ function SendClothForm(props) {
       methods={formMethods}
       onSubmit={handleSubmit(onSubmit)}
     >
-      <div className="grid gap-4 md:grid-cols-2">
-        <Form.Input field="name" label="Name" />
-        <Form.Input field="email" label="Email" />
+      <div className="mt-10 sm:mt-0">
+        <div className="mt-5 md:mt-0 md:col-span-2">
+          <div className="shadow overflow-hidden sm:rounded-md">
+            <div className="px-4 py-5 bg-white sm:p-6">
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-3">
+                  <Form.Input field="name" label="Name" />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3">
+                  <Form.Input field="email" label="Email address" />
+                </div>
+
+                <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                  <Form.Input field="city" label="City" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <Form.DragDrop type="file" field="theFiles" multiple label="Photos" />
       <Button type="submit" disabled={props.disabled}>

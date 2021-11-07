68c254222fc3dd5c4dbcff47a07c91d36842f096
AliakbarSu <aliakbar.su@hotmail.com>
2021-11-07 16:28:07 +1300

    Update drag drop form component to display correct label

--- components/ui/form.js
+++ components/ui/form.js
@@ -183,7 +183,7 @@ function FormDragDrop(props) {
   return (
     <div>
       <label className="block text-sm font-medium text-gray-700">
-        Cover photo
+        {props.label}
       </label>
       <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
         <div className="space-y-1 text-center">

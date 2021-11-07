70946773be5d1748df6fc8fe81b8980ac5b9f947
AliakbarSu <aliakbar.su@hotmail.com>
2021-11-07 15:36:01 +1300

    Update create cloth review api route

--- pages/api/graphcms/create-cloth-review.js
+++ pages/api/graphcms/create-cloth-review.js
@@ -1,11 +1,14 @@
 import nextConnect from 'next-connect'
 import multer from 'multer'
 import create_cloth_review_request from '@/lib/create-cloth-review'
+import upload_publish_assets from '@/lib/upload-publish-assets'
 const path = require('path')
+
+const UPLOAD_FOLDER = 'uploads'
 
 const upload = multer({
   storage: multer.diskStorage({
-    destination: path.join(__dirname, 'public', 'uploads'),
+    destination: path.join(__dirname, 'public', UPLOAD_FOLDER),
     filename: (req, file, cb) => cb(null, file.originalname)
   }),
   limits: {
@@ -30,11 +33,12 @@ const apiRoute = nextConnect({
 apiRoute.use(upload.array('theFiles'))
 
 apiRoute.post(async (req, res) => {
+  const uploadedAssets = await upload_publish_assets(UPLOAD_FOLDER)
   const customerData = {
     name: req.body.name,
     email: req.body.email,
     city: req.body.city,
-    photos: []
+    photos: { connect: uploadedAssets.map((asset) => ({ id: asset })) }
   }
   await create_cloth_review_request(customerData)
   res.status(200).json({ data: 'success' })

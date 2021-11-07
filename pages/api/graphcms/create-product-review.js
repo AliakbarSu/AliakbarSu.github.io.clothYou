43f4f60c1d227989fcd9eafbf844c9535547dcd9
AliakbarSu <aliakbar.su@hotmail.com>
2021-11-07 15:22:09 +1300

    feat: Update create product review to include photos

--- pages/api/graphcms/create-product-review.js
+++ pages/api/graphcms/create-product-review.js
@@ -1,6 +1,52 @@
+import nextConnect from 'next-connect'
+import multer from 'multer'
+const path = require('path')
 import graphcmsMutationClient, { gql } from '@/lib/graphcms-mutation-client'
+import uploadPublishAssets from '@/lib/upload-publish-assets'
 
-export default async (req, res) => {
+const UPLOAD_FOLDER = 'reviews'
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: path.join(__dirname, 'public', UPLOAD_FOLDER),
+    filename: (req, file, cb) => cb(null, file.originalname)
+  }),
+  limits: {
+    fieldNameSize: 500,
+    files: 5,
+    fields: 10,
+    fileSize: 200 * 1024 * 1024
+  }
+})
+
+const apiRoute = nextConnect({
+  onError(error, req, res) {
+    res
+      .status(501)
+      .json({ error: `Sorry something Happened! ${error.message}` })
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` })
+  }
+})
+
+apiRoute.use(upload.array('theFiles'))
+
+apiRoute.post(async (req, res) => {
+  const uploadedAssets = await uploadPublishAssets(UPLOAD_FOLDER)
+  const reviewData = {
+    headline: req.body.headline,
+    name: req.body.name,
+    email: req.body.email,
+    product: JSON.parse(req.body.product),
+    content: req.body.content,
+    photos: { connect: uploadedAssets.map((asset) => ({ id: asset })) }
+  }
+  const { review } = await create_publish_review(reviewData)
+  res.status(200).json({ review })
+})
+
+const create_publish_review = async (reviewData) => {
   try {
     const { review } = await graphcmsMutationClient.request(
       gql`
@@ -10,7 +56,7 @@ export default async (req, res) => {
           }
         }
       `,
-      { review: req.body }
+      { review: reviewData }
     )
 
     const { publishedReview } = await graphcmsMutationClient.request(
@@ -24,20 +70,27 @@ export default async (req, res) => {
             headline
             name
             rating
+            photos {
+              id
+              url
+            }
           }
         }
       `,
       { id: review.id }
     )
 
-    res.status(201).json({ review: publishedReview })
+    return { review: publishedReview }
   } catch (error) {
     console.log(error)
-    res
-      .status(500)
-      .json({
-        status: 500,
-        message: 'There was a problem submitting your review!'
-      })
+    throw error
+  }
+}
+
+export default apiRoute
+
+export const config = {
+  api: {
+    bodyParser: false // Disallow body parsing, consume as stream
   }
 }

9a9f7add3499f355b7d035b6c305acf6f5b7a656
AliakbarSu <aliakbar.su@hotmail.com>
2021-11-08 20:09:50 +1300

    Feat: Add filters to products page

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,57 @@ import FiltersPopup from '@/components/filters-popup'
 import { useModalContext } from '@/context/modal'
 
 function IndexPage({ products }) {
+  const [filteredProducts, setFilteredProducts] = React.useState([...products])
   const { modalState, setModalState } = useModalContext()
-  const onFiltersApplyHandler = (filters) => {
-    console.log(filters)
+
+  const is_color_in_colors = (color, colors) => {
+    const updatedColor = color.map((s) => s.toUpperCase())
+    const updatedColors = colors.map((s) => s.toUpperCase())
+    return colors.length
+      ? updatedColors.some((r) => updatedColor.indexOf(r) >= 0)
+      : true
   }
+
+  const is_size_in_sizes = (size, sizes) => {
+    const updatedSize = size.map((s) => s.toUpperCase())
+    const updatedSizes = sizes.map((s) => s.toUpperCase())
+    return sizes.length
+      ? updatedSizes.some((r) => updatedSize.indexOf(r) >= 0)
+      : true
+  }
+
+  const is_price_within_range = (price, min, max) => {
+    if (!price || price == 0 || (min == 0 && max == 0)) {
+      return true
+    }
+    return price >= min && price <= max
+  }
+
+  const onFiltersApplyHandler = ({ color, size, price: { min, max } }) => {
+    const colorsInFilters = color
+    const sizesInFilters = size
+
+    const updatedProducts = products.filter((pr) => {
+      const colors = pr.variants.map((vr) => vr.name)
+      const sizes = pr.variants.map((vr) => vr.name)
+      const colorCondition = is_color_in_colors(colors, colorsInFilters)
+      const sizeCondition = is_size_in_sizes(sizes, sizesInFilters)
+      const priceCondition = is_price_within_range(pr.price, min, max)
+      return colorCondition && sizeCondition && priceCondition
+    })
+
+    setFilteredProducts(updatedProducts)
+  }
+
   return (
     <React.Fragment>
-      {modalState && (
-        <FiltersPopup
-          onClose={() => setModalState(false)}
-          onApply={onFiltersApplyHandler}
-        />
-      )}
-      <ProductGrid products={products} />
+      <FiltersPopup
+        show={modalState}
+        onClose={() => setModalState(false)}
+        onApply={onFiltersApplyHandler}
+      />
+
+      <ProductGrid products={filteredProducts} />
     </React.Fragment>
   )
 }

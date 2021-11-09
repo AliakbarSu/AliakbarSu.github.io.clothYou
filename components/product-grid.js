e56fc2cd18ee07a947400c2da8ecfcc905775a47
AliakbarSu <aliakbar.su@hotmail.com>
2021-11-09 14:01:30 +1300

    Enable product gird component to accept classname from parent

--- components/product-grid.js
+++ components/product-grid.js
@@ -1,9 +1,17 @@
 import ProductCard from '@/components/product-card'
+import { useSettingsContext } from '@/context/settings'
+import classNames from '@/utils/class-name'
 
-function ProductGrid({ products }) {
+function ProductGrid({ products, className = '' }) {
+  const { activeCurrency } = useSettingsContext()
   return (
-    <div className="gap-8 grid sm:grid-cols-3 lg:grid-cols-6">
-      {products.map(ProductCard)}
+    <div
+      className={classNames(
+        'gap-8 grid sm:grid-cols-3 lg:grid-cols-6',
+        className
+      )}
+    >
+      {products.map((pr) => ProductCard({ ...pr, activeCurrency }))}
     </div>
   )
 }
